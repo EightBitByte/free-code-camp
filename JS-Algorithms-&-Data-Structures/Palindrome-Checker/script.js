017d57025a89c72cb342f800409c07d4efac2bb5
EightBitByte <apixelatedpug@gmail.com>
2024-01-20 12:33:52 -0800

    :sparkles: Update script to fit certification specifications

--- JS-Algorithms-&-Data-Structures/Palindrome-Checker/script.js
+++ JS-Algorithms-&-Data-Structures/Palindrome-Checker/script.js
@@ -6,11 +6,13 @@ const explanation = document.getElementById("result-expl");
 const correctText = "Congrats! It is a palindrome."
 const incorrectText = "It is not a palindrome. Boo."
 
-const alphaNumReg = /[\d\s,.:]/g;
+const alphaNumReg = /[^a-zA-Z0-9]/g;
 
 function checkPalindrome() {
-    if (textInput.value.length > 0)
+    if (textInput.value.length > 0) {
+        console.log(textInput.value.replaceAll(alphaNumReg, ''));
         displayResult(isPalindrome(textInput.value.replaceAll(alphaNumReg, '').toLowerCase()));
+    }
     else 
         alert("Please input a value");
 }
@@ -28,18 +30,22 @@ function displayResult(isCorrect) {
         result.classList.remove('boo')
         result.classList.add('congrats')
         result.innerHTML = `
-        <h1>CONGRATS! IT IS A PALINDROME.</h1>`
+        <span id="word"></span> is a palindrome.`
         explanation.innerHTML = `
         <h2>WHAT DOES THIS MEAN?</h2>
         <h3>THE STRING OF TEXT YOU ENTERED IS SPELLED THE SAME BACKWARDS AND FORWARDS.</h3>`
+        const wordSpan = document.getElementById("word");
+        wordSpan.innerText = textInput.value;
     } else {
         result.classList.remove('congrats')
         result.classList.add('boo')
         result.innerHTML = `
-        <h1>IT IS NOT A PALINDROME. BOO.</h1>`
+        <span id="word"></span> is not a palindrome.`
         explanation.innerHTML = `
         <h2>WHAT DOES THIS MEAN?</h2>
         <h3>THE STRING OF TEXT YOU ENTERED IS <span class="incorrect">NOT</span> SPELLED THE SAME BACKWARDS AND FORWARDS.</h3>` 
+        const wordSpan = document.getElementById("word");
+        wordSpan.innerText = textInput.value;
     }
 }
 
